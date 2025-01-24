159f3fd11b64b3ea205ddee82ebc1261b3ecb0b5
Khaled Ahmed <www.khaledah826@gmail.com>
2025-01-24 16:32:39 +0200

    jobify

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-   domains: ['i.postimg.cc', 'mdbcdn.b-cdn.net'], 
-
-  },
+    images: {
+        domains: ["i.postimg.cc", "mdbcdn.b-cdn.net"],
+    },
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...(config.resolve.alias || {}),
+            "@loadable/component": "next/dynamic",
+        };
+        return config;
+    },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
 };
-
 
 export default nextConfig;
